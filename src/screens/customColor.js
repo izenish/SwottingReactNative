d829bc06e901f4c4b2c73fa0515f574ed1480511
izenish <50284866+izenish@users.noreply.github.com>
2020-08-31 19:49:14 +0545

    change color of the box by increasing decreasing rgb calues

--- src/screens/customColor.js
+++ src/screens/customColor.js
@@ -1,28 +1,57 @@
 import React, { useState } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Alert } from "react-native";
 import ColorComponent from "../components/rgb";
 
 const customColor = function () {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
+  console.log(red);
+  const setColor = (color, change) => {
+    //color==="red","green","blue"
+    //change==+15,-15
+    switch (color) {
+      case "red":
+        red + change > 255 || red + change < 0
+          ? null && console.log("outofboubds")
+          : setRed(red + change);
+        break;
 
+      case "green":
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change);
+
+      default:
+        blue + change > 255 || blue + change < 0
+          ? null
+          : setBlue(blue + change);
+        break;
+    }
+  };
   return (
     <View>
       <ColorComponent
         color="Red"
-        onIncrement={() => setRed(red + 10)}
-        onDecrement={() => setRed(red - 10)}
+        onIncrement={() => setColor("red", 10)}
+        onDecrement={() => setColor("red", -10)}
       />
       <ColorComponent
         color="Green"
-        onIncrement={() => setGreen(green + 10)}
-        onDecrement={() => setGreen(green - 10)}
+        onIncrement={() => setColor("green", 10)}
+        onDecrement={() => setColor("green", -10)}
       />
       <ColorComponent
         color="Blue"
-        onIncrement={() => setBlue(blue + 10)}
-        onDecrement={() => setBlue(blue - 10)}
+        onIncrement={() => setColor("blue", 10)}
+        onDecrement={() => setColor("blue", -10)}
+      />
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red},${green},${blue})`,
+        }}
       />
     </View>
   );
